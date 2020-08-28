ff87866892d9319ee3c4b53813ca4fc27ab3d9c8
AbrahemGh1 <abrahem_gh@yahoo.com>
2020-08-28 17:26:51 +0300

    add footer, head,nav in seperated file  and add style to page

--- app.js
+++ app.js
@@ -7,19 +7,24 @@ app.listen(3000);
 
 
 app.get('/', (req, res) => {
-    res.render('index');
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+      ];
+    res.render('index',{title:"Home",blogs});
 });
 
 app.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about',{title:"about"});
 });
 app.get('/blogs/create', (req, res) => {
-    res.render('create');
+    res.render('create',{title:"Create new blog"});
 
 });
 
 app.use((req,res)=>{
-    res.status(404).render('404');
+    res.status(404).render('404',{title:"404"});
 
 });
 
